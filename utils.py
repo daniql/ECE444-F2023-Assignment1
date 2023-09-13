a6ff7f71f73d5719a8f2cb0be44c9ffdb2d8c903
Danny Ahmad <43858798+daniql@users.noreply.github.com>
2023-09-13 12:24:46 -0400

    accounted for negative inputs

--- utils.py
+++ utils.py
@@ -2,12 +2,19 @@ class utils:
     def reversed(self, num:int) -> int:
         if not type(num) is int:
             raise TypeError("input should be an int")
-        
-        ret = str(num)
-        return int(ret[::-1])
+        if num >= 0:
+            ret = str(num)
+            return int(ret[::-1])
+        else:
+            ret = str(num)[1:]
+            ret = '-'+ret[::-1]
+            return int(ret)
 
     def formatter(self, num:int):
         if not type(num) is int:
             raise TypeError("input should be an int")
         
         return bin(num), oct(num)
+    
+test = utils()
+print(test.reversed(-12345))
